a7ca37a2226bb5cc1a7f3c94e42db79cbc52c3f7
lehieuphuoc <lehieuphuoc35205@gmail.com>
2024-11-18 14:04:29 +0700

    Code da cap nhat

---
+++ home.php
@@ -0,0 +1,12 @@
+<!-- Trường cao đẳng fpt Thanh Hoá -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <h1>Trường Cao đẳng fpt Thanh Hoá</h1>
+ </body>
+ </html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- Lê Hiểu Phước_03/05/2005 -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <header>
+        <h1>Tôi là Phước</h1>
+    </header>
+    
+ </body>
+ </html>
